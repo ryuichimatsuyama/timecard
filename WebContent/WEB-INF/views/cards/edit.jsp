7abb5ccac8d33279466e304fe02cc4e5c16739e0
ryuichi.matsuyama <mwtpjk@yahoo.co.jp>
2021-03-26 19:53:02 +0900

    AddCardEdit

---
+++ WebContent/WEB-INF/views/cards/edit.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<c:import url="/WEB-INF/views/layout/app.jsp">
+	<c:param name="content">
+		<h2>承認と編集ページ</h2>
+
+		<form method="POST" action="<c:url value='/approvals/create'/>">
+<c:if test="${errors!=null }">
+	<div id="flush_error">
+		入力内容にエラーがあります。<br />
+		<c:forEach var="error" items="${errors }">
+・<c:out value="${error }" />
+			<br />
+		</c:forEach>
+	</div>
+</c:if>
+<label for="start_time">出勤時間</label>
+<br />
+<input type="time" name="start_time" value="${card.start}" />
+<br />
+<br />
+<label for="end_time">退勤時間</label>
+<br />
+<input type="time" name="end_time" value="${card.end }" />
+<br />
+<br />
+
+
+
+
+<label for="review">レビュー</label>
+<br />
+<textarea name="review" rows="10" cols="50">${approval.review}</textarea>
+<br/><br/>
+<input type="hidden" name="_token" value="${_token}" />
+<button type="submit">承認</button>
+		</form>
+
+		<p>
+			<a href="<c:url value='/approvals/index'/>">一覧に戻る</a>
+		</p>
+	</c:param>
+</c:import>
---
+++ src/controllers/card/CardEditServlet.java
@@ -0,0 +1,60 @@
+package controllers.card;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Approval;
+import models.Card;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class CardEditServlet
+ */
+@WebServlet("/card/edit")
+public class CardEditServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CardEditServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		EntityManager em = DBUtil.createEntityManager();
+		// セッションスコープから日報のidを取得して該当のidの日報１件のみをデータベースから取得
+		Card c = em.find(Card.class, Integer.parseInt(request.getParameter("id")));
+
+		Approval a = new Approval();
+		em.close();
+		request.setAttribute("approval", a);
+		request.setAttribute("_token", request.getSession().getId());
+		request.setAttribute("card", c);
+		request.getSession().setAttribute("card_id", c.getId());
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/cards/edit.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
